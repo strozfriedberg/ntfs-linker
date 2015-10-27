5e2f41e31f07f5ad24cef4c8b1d1827b1a6ecc52
Zack Weger <ZWeger@StrozFriedberg.com>
2015-10-27 09:44:33 -0400

    #29 add LGPL license headers

--- src/progress.cpp
+++ src/progress.cpp
@@ -1,3 +1,29 @@
+/*
+ * ntfs-linker
+ * Copyright 2015 Stroz Friedberg, LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the license, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * A copy of the GNU Lesser General Public License is available at
+ * <http://www.gnu.org/licenses/>.
+ *
+ * You can contact Stroz Friedberg by electronic and paper mail as follows:
+ *
+ * Stroz Friedberg, LLC
+ * 32 Avenue of the Americas
+ * 4th Floor
+ * New York, NY, 10013
+ * info@strozfriedberg.com
+ */
+
 #include "progress.h"
 
 #include <iomanip>
