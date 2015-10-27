e97a1d7f77422a463eb83b4f370965cde1e6e293
Zack Weger <ZWeger@StrozFriedberg.com>
2015-10-27 10:53:45 -0400

    Tidy up arg parsing

--- src/ntfs_linker.cpp
+++ src/ntfs_linker.cpp
@@ -31,9 +31,20 @@
 
 namespace po = boost::program_options;
 
-void printHelp(const po::options_description& desc) {
+void printHelp(const po::options_description& desc, const po::positional_options_description& posOpts) {
+  std::vector<std::string> parts;
+  parts.push_back("Usage: ntfs_linker");
+  for (unsigned int i = 0; i < posOpts.max_total_count(); ++i) {
+    parts.push_back(posOpts.name_for_position(i));
+  }
+  if (desc.options().size() > 0) {
+    parts.push_back("[options]");
+  }
+  std::stringstream ss;
+  std::copy(parts.begin(), parts.end(), std::ostream_iterator<std::string>(ss, " "));
   std::cout << "ntfs-linker, Copyright (c) Stroz Friedberg, LLC" << std::endl;
   std::cout << "Version " << VERSION << std::endl;
+  std::cout << ss.str() << std::endl;
   std::cout << desc << std::endl;
   std::cout << "Note: this program will also look for files named $J when looking for $UsnJrnl file." << std::endl;
 }
@@ -42,32 +53,35 @@ int main(int argc, char** argv) {
   Options opts;
 
   po::options_description desc("Allowed options");
+  po::positional_options_description posOpts;
+  posOpts.add("ntfs-dir", 1);
+  posOpts.add("output", 1);
   desc.add_options()
-    ("help", "display help and exit")
+    ("ntfs-dir", po::value<std::string>(), "If no image specified, location of root directory containing input files. Otherwise, root directory in which to dump files extracted from image. See the docs for info about ntfs-dir structure.")
     ("output", po::value<std::string>(), "directory in which to dump output files")
-    ("input", po::value<std::string>(), "If no image specified, location of directory containing input files: $MFT, $UsnJrnl, $LogFile. Otherwise, root directory in which to dump files extracted from image.")
-    ("image", po::value<std::vector<std::string>>(), "Path to image")
-    ("version", "display version number and exit")
+    ("image", po::value<std::vector<std::string>>(), "Path to image file(s)")
     ("overwrite", "overwrite files in the output directory. Default: append")
-    ("extra", "Outputs supplemental lower-level parsed data from $UsnJrnl and $LogFile");
+    ("extra", "Outputs supplemental lower-level parsed data from $UsnJrnl and $LogFile")
+    ("help", "display help and exit")
+    ("version", "display version number and exit");
 
   po::variables_map vm;
   try {
-    po::store(po::command_line_parser(argc, argv).options(desc).run(), vm);
+    po::store(po::command_line_parser(argc, argv).options(desc).positional(posOpts).run(), vm);
     po::notify(vm);
 
     opts.overwrite = vm.count("overwrite");
     opts.extra = vm.count("extra");
 
     if (vm.count("help")) {
-      printHelp(desc);
+      printHelp(desc, posOpts);
     }
     else if (vm.count("version")) {
         std::cout << "ntfs_linker version: " << VERSION << std::endl;
     }
-    else if (vm.count("input") && vm.count("output")) {
+    else if (vm.count("ntfs-dir") && vm.count("output")) {
       // Run
-      opts.input = fs::path(vm["input"].as<std::string>());
+      opts.input = fs::path(vm["ntfs-dir"].as<std::string>());
       opts.output = fs::path(vm["output"].as<std::string>());
       if (vm.count("image")) {
         opts.imgSegs = vm["image"].as<std::vector<std::string>>();
@@ -75,13 +89,13 @@ int main(int argc, char** argv) {
       run(opts);
     }
     else {
+      printHelp(desc, posOpts);
       std::cerr << "Error: did not understand arguments" << std::endl;
-      printHelp(desc);
     }
   }
   catch (std::exception& err) {
     std::cerr << "Error: " << err.what() << std::endl << std::endl;
-    printHelp(desc);
+    printHelp(desc, posOpts);
     return 1;
   }
   return 0;
